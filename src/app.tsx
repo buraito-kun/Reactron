6cf7b68e56532d47e1d50d02c62228edc0828d49
buraito-kun <brightrock19220@outlook.co.th>
2024-11-02 19:31:31 +0700

    😭 mistake: Remove react-router-dom
    cause Electron is a Single Page Application (SPA)

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,10 @@
-import React from "react"
-import { createRoot } from 'react-dom/client';
-import { HashRouter, Route, Routes } from "react-router-dom";
+import React from "react";
+import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.body);
 root.render(
-  <HashRouter>
-    <Routes>
-      <Route path="/" element={<div><h1>💖 Hello World!</h1><p>Welcome to your Electron application.</p></div>} />
-    </Routes>
-  </HashRouter>
+  <div>
+    <h1>💖 Hello World!</h1>
+    <p>Welcome to your Electron application.</p>
+  </div>
 );
